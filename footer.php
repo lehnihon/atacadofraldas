af46eb49b3fddb6322e1899fcabf53f3af0dadfc
Leandro <lehnihon@hotmail.com>
2015-11-28 13:08:39 -0200

    home pronta

--- footer.php
+++ footer.php
@@ -1,22 +1,54 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package site
- */
-
-?>
-
 	<footer id="footer" role="contentinfo">
-		<div class="container">
-			<div class="row">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'site' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'site' ), 'WordPress' ); ?></a>
-				<span class="sep"> | </span>
-				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'site' ), 'site', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-			</div><!-- .row -->
-		</div><!-- .container -->
+		<div class="footer-content">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<p>
+							<strong>Sobre o Atacado das Fraldas Indaiatuba.</strong><br><br>
+							No Atacado das Fraldas Indaiatuba você encontra uma linha completa de fraldas para crianças e adultos com preços que cabem no seu bolso.
+							<br>Além de bolsas, absorventes, leite, acessórios e roupas para bebês.
+							<br><br>
+							<strong>Endereço</strong>
+							<br><br>Avenida Ário Barnabé, XXXX - Jardim Morada do Sol 
+							<br>Indaiatuba - São Paulo
+						</p>
+					</div>
+					<div class="col-md-2 col-md-offset-1">
+						<strong>Produtos</strong><br><br>
+						<ul>
+							<?php
+							$args = array(
+								'posts_per_page' => 10,
+							    'orderby' => 'rand');
+							$query = new WP_Query( $args );
+
+							while ( $query->have_posts() ) : $query->the_post(); 
+							?>
+								<?php if($query->current_post == 5): ?>
+									<?php echo '</div><div class="col-md-2"><br><br><ul>'; ?>
+								<?php endif; ?>
+								<li><a href="<?php the_permalink(); ?>"><?php the_title( );?></a></li>
+							<?php endwhile; ?>
+						</ul>	
+					</div>
+					<div class="col-md-3">
+						<strong>Receba nossas novidades</strong>
+					</div>
+				</div><!-- .row -->
+			</div><!-- .container -->			
+		</div>
+		<div class="copyrino">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10">
+						&copy; <?php echo date('Y') ?> Atacado das Fraldas Indaiatuba | <a target="_blank" href="http://agenciarino.com.br/">Site desenvolvido pela Agência Rino.</a>
+					</div>
+					<div class="col-md-2">
+						<a target="_blank" href="http://agenciarino.com.br/"><img class="img-responsive" src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/logo_rino.png"?>" /></a>	
+					</div>
+				</div>	
+			</div>	
+		</div>
 	</footer><!-- #footer -->
 </div><!-- #page -->
 
--- index.php
+++ index.php
@@ -1,59 +1,40 @@
 <?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package site
- */
-
 get_header(); ?>
 
 <div id="content">
-	<div class="container">
-		<div class="row">
-			<main class="col-md-8">
+	<section id="banner">
+		<div class="container">
+			<div class="row banner-row">
+				<div class="col-md-12">			
+					<?php putRevSlider( "banner-index" ) ?>
+				</div>
+			</div>
+			<div class="row categorias-row">
+				<div class="col-md-4">		
+					<?php putRevSlider( "categoria1" ) ?>
+				</div>
+				<div class="col-md-4">		
+					<?php putRevSlider( "categoria1" ) ?>
+				</div>
+				<div class="col-md-4">		
+					<?php putRevSlider( "categoria1" ) ?>
+				</div>
+			</div>
+		</div>
+	</section>
 
-			<?php if ( have_posts() ) : ?>
+	<section id="marcas">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">			
+					<?php putRevSlider( "marcas" ) ?>
+				</div>
+			</div>
+		</div>
+	</section>
 
-				<?php if ( is_home() && ! is_front_page() ) : ?>
-					<header>
-						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-					</header>
-				<?php endif; ?>
+	<?php get_template_part( 'template-parts/redes-bot'); ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
-
-				<?php the_posts_navigation(); ?>
-
-			<?php else : ?>
-
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-			<?php endif; ?>
-			</main><!-- #main -->
-			<aside class="col-md-4">
-				<?php get_sidebar(); ?>
-			</aside>
-		</div><!-- .row -->
-	</div><!-- .container -->
-</div><!-- #content -->
+</div>
 
 <?php get_footer(); ?>
